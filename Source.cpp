c1b74ced38ec913e8394d7f688bcee69b40399a7
obegrand <obevan7777@yandex.ru>
2023-12-22 15:16:37 +0300

    v 1.1

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,12 @@
+//for YandexPracticum from obegrand - Andrey Melnikov
 #include <iostream>
 
 int main() {
-	std::cout << "Hello from C++! ^_^\n";
+	std::cout << "The quote from \"Hamlet\":" << std::endl;
+	std::cout << "To be, or not to be, that is the question:" << std::endl;
+	std::cout << "Whether 'tis nobler in the mind to suffer" << std::endl;
+	std::cout << "The slings and arrows of outrageous fortune," << std::endl;
+	std::cout << "Or to take arms against a sea of troubles" << std::endl;
+	std::cout << "And by opposing end them." << std::endl;
 	system("pause");
 }
