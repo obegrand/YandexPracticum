06585a912cf4f60980a122777491bdacb0e7e586
obegrand <obevan7777@yandex.ru>
2023-12-22 15:57:49 +0300

    v 1.2.1

--- Source.cpp
+++ Source.cpp
@@ -7,8 +7,8 @@ int main() {
 	srand(time(NULL));
 	for (size_t i = 0; i < 5; i++)
 	{
-		int x = rand() % 20;
-		std::cout << "Number " << x << " is between " << x - 1 << " and " << x + 1 << ". doubled " << x << " is " << x + x << "." << std::endl;
+		int x = rand() % 99 + 1;
+		std::cout << "Number " << x << " is between " << x - 1 << " and " << x + 1 << ". Doubled " << x << " is " << x + x << "." << std::endl;
 	}
 	system("pause");
 }
