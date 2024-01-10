98e13f771a3c9ac8f8caf7ec9a5835e9c8f50547
obegrand <obevan7777@yandex.ru>
2024-01-10 17:39:22 +0300

    #21

--- Source.cpp
+++ Source.cpp
@@ -7,8 +7,40 @@
 
 using namespace std;
 
+string GetWord() {
+	string str;
+	string result_word;
+	getline(cin, str);
+	if (str.size() == 0)
+	{
+		return "";
+	}
+	else {
+		result_word += str[0];
+		for (size_t i = 1; i < str.size(); i++)
+		{
+			if (str[i]!=' ' && str[i-1]==' ')
+			{
+				result_word += str[i];
+			}
+		}
+		return result_word;
+	}
+}
+
+int GetIDONTKNOW() {
+	int x, y;
+	cin >> x >> y;
+	int sum = x + y;
+	int sum_size = (to_string(sum)).size();
+	return sum * sum_size;
+}
+
 int main() {
 	srand(time(NULL));
-	
+	int a, b;
+	cin >> a >> b;
+	if (a == b) { cout << "equal"s << endl; }
+	else { cout << "not equal"s << endl; }
 	system("pause");
 }
