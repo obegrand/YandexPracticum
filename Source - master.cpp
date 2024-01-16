417edb6e2c36c25a0d9a98784b6c7e5a7ff88a30
obegrand <obevan7777@yandex.ru>
2024-01-16 03:24:11 +0300

    16.01

--- Source - master.cpp
+++ Source - master.cpp
@@ -30,12 +30,47 @@ std::vector<std::string> GetWords(std::string query) {
 	return words;
 }
 
-int main() {
-	string query;
-	getline(cin, query);
-	for (string wordw : GetWords(query)) {
-		cout << wordw << " ";
+void IsPalindrome(string s) {
+	if (s.size() <= 1)
+	{
+		cout << "1"s << endl;
 	}
-	cout << endl;
+	else {
+		int counter = 0;
+		for (int i = 0; i < s.size() / 2; i++)
+		{
+			if (s[i] == s[s.size() -1 - i])
+			{
+				++counter;
+			}
+		}
+		if (counter == s.size()/2)
+		{
+			cout << "1"s << endl;
+		}
+		else
+		{
+			cout << "0"s << endl;
+		}
+	}
+}
+
+int Factorial(int x) {
+	if (x == 0) { return 1; }
+	bool minus = false;
+	int rez = 1;
+	if (x < 0) { minus = true; x *= -1; }
+	for (int i = 1; i < x + 1; i++) {
+		rez *= i;
+	}
+	if (minus && x % 2 == true) { return rez * -1; }
+	return rez;
+}
+
+int main() {
+	for (int i = -10; i < 10; i++)
+	{
+		cout << Factorial(i) << endl;
+	}
 	system("pause");
 }
