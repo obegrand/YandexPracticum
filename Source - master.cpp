b86860a187607ce2143a628babc141a474aebd54
obegrand <obevan7777@yandex.ru>
2024-01-19 21:36:28 +0300

    19.01 final

--- Source - master.cpp
+++ Source - master.cpp
@@ -1,39 +1,30 @@
 //for YandexPracticum from obegrand aka Andrey Melnikov
+#include <algorithm>
 #include <iostream>
-#include <vector>
 #include <set>
 #include <string>
-#include <ctime>
-#include <iomanip>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-set<string> MakeSet(vector<string> query_words);
-vector<string> MakeVector(set<string> query_words);
-vector<string> SplitIntoWords(const string& text);
-set<string> ParseStopWords(const string& text);
-vector<string> ParseQuery(string text, set<string> stop_words);
+string ReadLine() {
+    string s;
+    getline(cin, s);
+    return s;
+}
 
-int main() {
-    // Read stop words
-    string stop_words_joined;
-    getline(cin, stop_words_joined);
-    set<string> stop_words = ParseStopWords(stop_words_joined);
-
-    // Read query
-    string query;
-    getline(cin, query);
-    vector<string> query_words = ParseQuery(query, stop_words);
-
-    for (string word : query_words) {
-        cout << '[' << word << ']' << endl;
-    }
+int ReadLineWithNumber() {
+    int result = 0;
+    cin >> result;
+    ReadLine();
+    return result;
 }
 
 vector<string> SplitIntoWords(const string& text) {
     vector<string> words;
     string word;
-    for (char c : text) {
+    for (const char c : text) {
         if (c == ' ') {
             if (!word.empty()) {
                 words.push_back(word);
@@ -47,33 +38,79 @@ vector<string> SplitIntoWords(const string& text) {
     if (!word.empty()) {
         words.push_back(word);
     }
+
     return words;
 }
 
 set<string> ParseStopWords(const string& text) {
     set<string> stop_words;
-    for (string& word : SplitIntoWords(text)) {
+    for (const string& word : SplitIntoWords(text)) {
         stop_words.insert(word);
     }
     return stop_words;
 }
 
-vector<string> ParseQuery(string text, set<string> stop_words) {
+vector<string> SplitIntoWordsNoStop(const string& text, const set<string>& stop_words) {
     vector<string> words;
-    for (string word : SplitIntoWords(text)) {
+    // проходим по всем словам из текста и проверяем, что их нет в списке стоп-слов
+    for (const string& word : SplitIntoWords(text)) {
         if (stop_words.count(word) == 0) {
             words.push_back(word);
         }
     }
+    // вернём результат без стоп-слов
     return words;
 }
 
-set<string> MakeSet(vector<string> query_words) {
-	set<string> result(query_words.begin(), query_words.end());
-	return result;
+void AddDocument(vector<vector<string>>& documents, const set<string>& stop_words,
+    const string& document) {
+    const vector<string> words = SplitIntoWordsNoStop(document, stop_words);
+    documents.push_back(words);
 }
 
-vector<string> MakeVector(set<string> query_words) {
-	vector<string> result(query_words.begin(), query_words.end());
-	return result;
+// Разбирает text на слова и возвращает только те из них, которые не входят в stop_words
+set<string> ParseQuery(const string& text, const set<string>& stop_words) {
+    set<string> query_words;
+
+    // Напишите код функции
+
+    return query_words;
+}
+
+// Возвращает true, если среди слов документа (document_words)
+// встречаются слова поискового запроса query_words
+bool MatchDocument(const vector<string>& document_words, const set<string>& query_words) {
+    // Напишите код функции
+
+    return false;
+}
+
+// Возвращает массив id документов, подходящих под запрос query
+// Стоп-слова исключаются из поиска
+vector<int> FindDocuments(const vector<vector<string>>& documents, const set<string>& stop_words,
+    const string& query) {
+    vector<int> matched_documents;
+
+    // Напишите код функции
+    // Воспользуйте вспомогательными функциями ParseQuery, MatchDocument
+    // В качестве id документа используйте его индекс в массиве documents
+
+    return matched_documents;
+}
+
+int main() {
+    const string stop_words_joined = ReadLine();
+    const set<string> stop_words = ParseStopWords(stop_words_joined);
+
+    // Read documents
+    vector<vector<string>> documents;
+    const int document_count = ReadLineWithNumber();
+    for (int document_id = 0; document_id < document_count; ++document_id) {
+        AddDocument(documents, stop_words, ReadLine());
+    }
+
+    const string query = ReadLine();
+    for (const int document_id : FindDocuments(documents, stop_words, query)) {
+        cout << "{ document_id = "s << document_id << " }"s << endl;
+    }
 }
