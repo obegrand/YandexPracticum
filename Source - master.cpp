9cec8075318cebfa1e1fe217f81d950a98270256
obegrand <obevan7777@yandex.ru>
2024-01-19 18:16:09 +0300

    19.01

--- Source - master.cpp
+++ Source - master.cpp
@@ -1,61 +1,76 @@
 //for YandexPracticum from obegrand aka Andrey Melnikov
 #include <iostream>
 #include <vector>
+#include <set>
 #include <string>
 #include <ctime>
 #include <iomanip>
 
 using namespace std;
 
-std::vector<std::string> GetWords(std::string query) {
-	std::vector<std::string> words;
-	std::string word;
-	for (char c : query) {
+//bool IsPalindrome(string s) {
+//	for (int i = 0; i < s.size() / 2; ++i) {
+//		if (s[i] != s[s.size() - i - 1]) {
+//			return false;
+//		}
+//	}
+//	return true;
+//}
+//
+//vector<string> PalindromeFilter(vector<string> words, int min_length) {
+//	vector<string> filtered_words;
+//	for (string word : words)
+//	{
+//		if (IsPalindrome(word) && word.size() >= min_length)
+//		{
+//			filtered_words.push_back(word);
+//		}
+//	}
+//	return filtered_words;
+//}
+//
+//string query;
+//getline(cin, query);
+//vector<string> words = GetWords(query);
+//vector<string> palidrom_words = PalindromeFilter(words, 3);
+
+int CalculateSimilarity(vector<string> first, vector<string> second) {
+	// верните размер пересечения множеств слов first и second
+	set<string> similarityWords;
+	for (string word1 : first) {
+		for (string word2 : second) {
+			if (word1 == word2) { similarityWords.insert(word2); }
+		}
+	}
+	return similarityWords.size();
+}
+
+vector<string> SplitIntoWords(string text) {
+	vector<string> words;
+	string word;
+	for (const char c : text) {
 		if (c == ' ') {
-			if (word != "") {  //првоерка на пустоту 
+			if (!word.empty()) {
 				words.push_back(word);
-				word = "";
+				word.clear();
 			}
 		}
 		else {
 			word += c;
 		}
 	}
-	if (word != "") {  //добавляем последнее слово
+	if (!word.empty()) {
 		words.push_back(word);
 	}
 	return words;
 }
 
-bool IsPalindrome(string s) {
-	for (int i = 0; i < s.size() / 2; ++i) {
-		if (s[i] != s[s.size() - i - 1]) {
-			return false;
-		}
-	}
-	return true;
-}
-
-vector<string> PalindromeFilter(vector<string> words, int min_length) {
-	vector<string> filtered_words;
-	for (string word : words)
-	{
-		if (IsPalindrome(word) && word.size() >= min_length)
-		{
-			filtered_words.push_back(word);
-		}
-	}
-	return filtered_words;
-}
-
 int main() {
-	string query;
+	string query, description;
+
 	getline(cin, query);
-	vector<string> words = GetWords(query);
-	vector<string> palidrom_words = PalindromeFilter(words, 3);
-	for (size_t i = 0; i < palidrom_words.size(); i++)
-	{
-		cout << palidrom_words[i] << endl;
-	}
+	getline(cin, description);
+
+	cout << CalculateSimilarity(SplitIntoWords(query), SplitIntoWords(description)) << endl;
 	system("pause");
 }
