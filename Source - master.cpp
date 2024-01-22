c1b9219125dd5e7aae449fb183c4aa3578417acf
obegrand <obevan7777@yandex.ru>
2024-01-22 18:31:13 +0300

    22.01

--- Source - master.cpp
+++ Source - master.cpp
@@ -5,8 +5,28 @@
 
 using namespace std;
 
+void PrintMap(const map<char, int>& map_tmp) {
+    for (const auto& [key, value] : map_tmp){
+        cout << key << " - " << value << endl;
+    }
+}
+
+map<char, int> GetAnagram(const string& str) {
+    map<char, int> tmp_map;
+    for (const char& tmp_char : str) {
+        ++tmp_map[tmp_char];
+    }
+    PrintMap(tmp_map);
+    return tmp_map;
+}
+
 bool CheckAnagram(const string& s1, const string& s2) {
-    // напишите решение тут
+    if (GetAnagram(s1) == GetAnagram(s2)){
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
 void CheckIsTrue(bool value) {
@@ -20,7 +40,10 @@ void CheckIsTrue(bool value) {
 
 int main() {
     CheckIsTrue(CheckAnagram("tea"s, "eat"s));
-    CheckIsTrue(!CheckAnagram("battle"s, "beatle"s));
-    CheckIsTrue(!CheckAnagram("lift"s, "elevator"s));
-    CheckIsTrue(CheckAnagram("ocean"s, "canoe"s));
+
+    system("pause");
+    //CheckIsTrue(CheckAnagram("tea"s, "eat"s));
+    //CheckIsTrue(!CheckAnagram("battle"s, "beatle"s));
+    //CheckIsTrue(!CheckAnagram("lift"s, "elevator"s));
+    //CheckIsTrue(CheckAnagram("ocean"s, "canoe"s));
 }
