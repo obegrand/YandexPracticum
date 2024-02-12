2f8bb8a7bac657c65b6faf5fc00f1abe30cb4b79
obegrand <obevan7777@yandex.ru>
2024-02-12 18:23:09 +0300

    Урок 6: Функциональные объекты Задание 2

--- Source - master.cpp
+++ Source - master.cpp
@@ -1,101 +1,42 @@
 //for YandexPracticum from obegrand aka Andrey Melnikov
+#include <algorithm>
 #include <iostream>
-#include <map>
-#include <string>
-#include <tuple>
 #include <vector>
 
 using namespace std;
 
-// Перечислимый тип для статуса задачи
-enum class TaskStatus {
-	NEW,          // новая
-	IN_PROGRESS,  // в разработке
-	TESTING,      // на тестировании
-	DONE          // завершена
+struct Animal {
+	string name;
+	int age;
+	double weight;
 };
 
-// Объявляем тип-синоним для map<TaskStatus, int>,
-// позволяющего хранить количество задач каждого статуса
-using TasksInfo = map<TaskStatus, int>;
-
-void PrintTasksInfo(TasksInfo tasks_info) {
-	cout << tasks_info[TaskStatus::NEW] << " new tasks"s
-		<< ", "s << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress"s
-		<< ", "s << tasks_info[TaskStatus::TESTING] << " tasks are being tested"s
-		<< ", "s << tasks_info[TaskStatus::DONE] << " tasks are done"s << endl;
+template <typename Container, typename KeyMapper>
+void SortBy(Container& container, KeyMapper key_mapper, bool reverse = false) {
+	return sort(container.begin(), container.end(), [key_mapper, reverse](const auto& lhs, const auto& rhs) {
+		if (reverse) { return key_mapper(lhs) > key_mapper(rhs); }
+		return key_mapper(lhs) < key_mapper(rhs);
+		});
 }
 
-class TeamTasks {
-public:
-	// Добавить новую задачу (в статусе NEW) для конкретного разработчитка
-	void AddNewTask(const string& person) {
-		++developers_[person][TaskStatus::NEW];
+void PrintNames(const vector<Animal>& animals) {
+	for (const Animal& animal : animals) {
+		cout << animal.name << ' ';
 	}
-
-	// Получить статистику по статусам задач конкретного разработчика
-	const TasksInfo& GetPersonTasksInfo(const string& person) const {
-		return developers_.at(person);
-	}
-
-	// Обновить статусы по данному количеству задач конкретного разработчика,
-	// подробности см. ниже
-	tuple<TasksInfo, TasksInfo> PerformPersonTasks(const string& person, int task_count) {
-		TasksInfo updated_tasks, untouched_tasks;
-		TasksInfo& tasks = developers_[person];
-
-		for (auto count = static_cast<int>(TaskStatus::NEW); count < static_cast<int>(TaskStatus::DONE); ++count) {
-			updated_tasks[static_cast<TaskStatus>(count + 1)] = min(task_count, tasks[static_cast<TaskStatus>(count)]);
-			task_count -= updated_tasks[static_cast<TaskStatus>(count + 1)];
-		}
-		for (auto count = static_cast<int>(TaskStatus::NEW); count < static_cast<int>(TaskStatus::DONE); ++count) {
-			untouched_tasks[static_cast<TaskStatus>(count)] = tasks[static_cast<TaskStatus>(count)] - updated_tasks[static_cast<TaskStatus>(count + 1)];
-			tasks[static_cast<TaskStatus>(count)] += updated_tasks[static_cast<TaskStatus>(count)] - updated_tasks[static_cast<TaskStatus>(count + 1)];
-		}
-		tasks[TaskStatus::DONE] += updated_tasks[TaskStatus::DONE];
-
-		RemoveZeros(updated_tasks);
-		RemoveZeros(untouched_tasks);
-		RemoveZeros(developers_.at(person));
-
-		return { updated_tasks, untouched_tasks };
-	}
-private:
-	map<string, TasksInfo> developers_;
-
-	void RemoveZeros(TasksInfo& tasks_info) {
-		// Соберём те статусы, которые нужно убрать из словаря
-		vector<TaskStatus> statuses_to_remove;
-		for (const auto& task_item : tasks_info) {
-			if (task_item.second == 0) {
-				statuses_to_remove.push_back(task_item.first);
-			}
-		}
-		for (const TaskStatus status : statuses_to_remove) {
-			tasks_info.erase(status);
-		}
-	}
-};
+	cout << endl;
+}
 
 int main() {
-	TeamTasks tasks;
-	for (int i = 0; i < 3; ++i) {
-		tasks.AddNewTask("Ivan"s);
-	}
-	cout << "Ivan's tasks: "s;
-	PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"s));
-	TasksInfo updated_tasks, untouched_tasks;
-
-	tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
-	cout << "Updated Ivan's tasks: "s;
-	PrintTasksInfo(updated_tasks);
-	cout << "Untouched Ivan's tasks: "s;
-	PrintTasksInfo(untouched_tasks);
-
-	tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
-	cout << "Updated Ivan's tasks: "s;
-	PrintTasksInfo(updated_tasks);
-	cout << "Untouched Ivan's tasks: "s;
-	PrintTasksInfo(untouched_tasks);
-	system("pause");
+    vector<Animal> animals = {
+        {"Мурка"s,   10, 5},
+        {"Белка"s,   5,  1.5},
+        {"Георгий"s, 2,  4.5},
+        {"Рюрик"s,   12, 3.1},
+    };
+    PrintNames(animals);
+    SortBy(animals, [](const Animal& animal) { return animal.name; }, true);
+    PrintNames(animals);
+    SortBy(animals, [](const Animal& animal) { return animal.weight; });
+    PrintNames(animals);
+    return 0;
 }
