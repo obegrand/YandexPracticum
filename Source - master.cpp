86ff2413b7ed3b519d11c885e86a3e424ef84373
obegrand <obevan7777@yandex.ru>
2024-02-27 20:49:29 +0300

    class Rational

--- Source - master.cpp
+++ Source - master.cpp
@@ -1,278 +1,100 @@
-//for YandexPracticum from obegrand aka Andrey Melnikov
-#include <algorithm>
-#include <cmath>
-#include <iostream>
-#include <map>
-#include <set>
-#include <string>
-#include <utility>
+#include <numeric>
+#include <ostream>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-const int MAX_RESULT_DOCUMENT_COUNT = 5;
-
-string ReadLine() {
-	string s;
-	getline(cin, s);
-	return s;
-}
-
-int ReadLineWithNumber() {
-	int result;
-	cin >> result;
-	ReadLine();
-	return result;
-}
-
-vector<string> SplitIntoWords(const string& text) {
-	vector<string> words;
-	string word;
-	for (const char c : text) {
-		if (c == ' ') {
-			if (!word.empty()) {
-				words.push_back(word);
-				word.clear();
-			}
-		}
-		else {
-			word += c;
-		}
-	}
-	if (!word.empty()) {
-		words.push_back(word);
-	}
-
-	return words;
-}
-
-struct Document {
-	Document() = default;
-	Document(const int& tmp_id, const double& tmp_relevance, const int& tmp_rating) :id(tmp_id), relevance(tmp_relevance), rating(tmp_rating) {}
-
-	int id = 0;
-	double relevance = 0;
-	int rating = 0;
-};
-
-enum class DocumentStatus {
-	ACTUAL,
-	IRRELEVANT,
-	BANNED,
-	REMOVED,
-};
-
-class SearchServer {
+// Класс Дроби в виде *numerator* / *denominator*
+class Rational {
 public:
-	SearchServer() = default;
+    // создание дроби 0/1
+    Rational() = default;
 
-	explicit SearchServer(const string& stop_words) {
-		for (const string& word : SplitIntoWords(stop_words)) {
-			stop_words_.insert(word);
-		}
-	}
+    // создание дроби *numerator*/1 
+    Rational(int numerator)
+        : numerator_(numerator)
+        , denominator_(1) {
+    }
 
-	template<typename conteiner_stop_words>
-	explicit SearchServer(const conteiner_stop_words& stop_words) {
-		for (const string& word : stop_words) {
-			stop_words_.insert(word);
-		}
-	}
+    // создание дроби *numerator* / *denominator*
+    Rational(int numerator, int denominator)
+        : numerator_(numerator)
+        , denominator_(denominator) {
+        Normalize();
+    }
 
-	void AddDocument(int document_id, const string& document, DocumentStatus status,
-		const vector<int>& ratings) {
-		const vector<string> words = SplitIntoWordsNoStop(document);
-		const double inv_word_count = 1.0 / words.size();
-		for (const string& word : words) {
-			word_to_document_freqs_[word][document_id] += inv_word_count;
-		}
-		documents_.emplace(document_id, DocumentData{ ComputeAverageRating(ratings), status });
-	}
+    // получение *numerator*
+    int Numerator() const {
+        return numerator_;
+    }
 
-	template<typename DocumentPredicate>
-	vector<Document> FindTopDocuments(const string& raw_query, DocumentPredicate predicate) const {
-		const Query query = ParseQuery(raw_query);
-		auto matched_documents = FindAllDocuments(query, predicate);
-
-		sort(matched_documents.begin(), matched_documents.end(),
-			[](const Document& lhs, const Document& rhs) {
-				if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
-					return lhs.rating > rhs.rating;
-				}
-				else {
-					return lhs.relevance > rhs.relevance;
-				}
-			});
-		if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
-			matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
-		}
-		return matched_documents;
-	}
-
-	vector<Document> FindTopDocuments(const string& raw_query) const {
-		return FindTopDocuments(raw_query, DocumentStatus::ACTUAL);
-	}
-
-	vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status) const {
-		return FindTopDocuments(raw_query,
-			[&status](int document_id, DocumentStatus new_status, int rating) {
-				return new_status == status;
-			});
-	}
-
-	int GetDocumentCount() const {
-		return documents_.size();
-	}
-
-	tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query,
-		int document_id) const {
-		const Query query = ParseQuery(raw_query);
-		vector<string> matched_words;
-		for (const string& word : query.plus_words) {
-			if (word_to_document_freqs_.count(word) == 0) {
-				continue;
-			}
-			if (word_to_document_freqs_.at(word).count(document_id)) {
-				matched_words.push_back(word);
-			}
-		}
-		for (const string& word : query.minus_words) {
-			if (word_to_document_freqs_.count(word) == 0) {
-				continue;
-			}
-			if (word_to_document_freqs_.at(word).count(document_id)) {
-				matched_words.clear();
-				break;
-			}
-		}
-		return { matched_words, documents_.at(document_id).status };
-	}
-
+    // получение *denominator*
+    int Denominator() const {
+        return denominator_;
+    }
 private:
-	struct DocumentData {
-		int rating;
-		DocumentStatus status;
-	};
+    // приведение к НОД(Наибольшему общему делителю)
+    void Normalize() {
+        if (denominator_ < 0) {
+            numerator_ = -numerator_;
+            denominator_ = -denominator_;
+        }
+        const int divisor = gcd(numerator_, denominator_);
+        numerator_ /= divisor;
+        denominator_ /= divisor;
+    }
 
-	set<string> stop_words_;
-	map<string, map<int, double>> word_to_document_freqs_;
-	map<int, DocumentData> documents_;
-
-	bool IsStopWord(const string& word) const {
-		return stop_words_.count(word) > 0;
-	}
-
-	vector<string> SplitIntoWordsNoStop(const string& text) const {
-		vector<string> words;
-		for (const string& word : SplitIntoWords(text)) {
-			if (!IsStopWord(word)) {
-				words.push_back(word);
-			}
-		}
-		return words;
-	}
-
-	static int ComputeAverageRating(const vector<int>& ratings) {
-		if (ratings.empty()) {
-			return 0;
-		}
-		int rating_sum = 0;
-		for (const int rating : ratings) {
-			rating_sum += rating;
-		}
-		return rating_sum / static_cast<int>(ratings.size());
-	}
-
-	struct QueryWord {
-		string data;
-		bool is_minus;
-		bool is_stop;
-	};
-
-	QueryWord ParseQueryWord(string text) const {
-		bool is_minus = false;
-		// Word shouldn't be empty
-		if (text[0] == '-') {
-			is_minus = true;
-			text = text.substr(1);
-		}
-		return { text, is_minus, IsStopWord(text) };
-	}
-
-	struct Query {
-		set<string> plus_words;
-		set<string> minus_words;
-	};
-
-	Query ParseQuery(const string& text) const {
-		Query query;
-		for (const string& word : SplitIntoWords(text)) {
-			const QueryWord query_word = ParseQueryWord(word);
-			if (!query_word.is_stop) {
-				if (query_word.is_minus) {
-					query.minus_words.insert(query_word.data);
-				}
-				else {
-					query.plus_words.insert(query_word.data);
-				}
-			}
-		}
-		return query;
-	}
-
-	// Existence required
-	double ComputeWordInverseDocumentFreq(const string& word) const {
-		return log(GetDocumentCount() * 1.0 / word_to_document_freqs_.at(word).size());
-	}
-
-	template<typename DocumentPredicate>
-	vector<Document> FindAllDocuments(const Query& query, DocumentPredicate predicate) const {
-		map<int, double> document_to_relevance;
-		for (const string& word : query.plus_words) {
-			if (word_to_document_freqs_.count(word) == 0) {
-				continue;
-			}
-			for (const auto& [document_id, term_freq] : word_to_document_freqs_.at(word)) {
-				if (predicate(document_id, documents_.at(document_id).status, documents_.at(document_id).rating)) {
-					const double IDF = log(static_cast<double>(documents_.size()) / word_to_document_freqs_.at(word).size());
-					document_to_relevance[document_id] += IDF * term_freq;
-				}
-			}
-		}
-
-		for (const string& word : query.minus_words) {
-			if (word_to_document_freqs_.count(word) == 0) {
-				continue;
-			}
-			for (const auto& [document_id, _] : word_to_document_freqs_.at(word)) {
-				document_to_relevance.erase(document_id);
-			}
-		}
-
-		vector<Document> matched_documents;
-		for (const auto& [document_id, relevance] : document_to_relevance) {
-			matched_documents.push_back({ document_id, relevance, documents_.at(document_id).rating });
-		}
-		return matched_documents;
-	}
+    int numerator_ = 0;
+    int denominator_ = 1;
 };
 
-void PrintDocument(const Document& document) {
-	cout << "{ "s
-		<< "document_id = "s << document.id << ", "s
-		<< "relevance = "s << document.relevance << ", "s
-		<< "rating = "s << document.rating
-		<< " }"s << endl;
+// вывод
+ostream& operator<<(ostream& output, Rational rational) {
+    output << rational.Numerator() << "/"s << rational.Denominator();
+    return output;
+}
+
+// ввод
+istream& operator>>(istream& input, Rational& rational) {
+    int numer, denominat;
+    char drob;
+    input >> numer >> drob >> denominat;
+    rational = Rational{ numer, denominat };
+    return input;
+}
+
+// сложение дробей
+Rational operator+(Rational left, Rational right) {
+    const int numerator = left.Numerator() * right.Denominator() + right.Numerator() * left.Denominator();
+    const int denominator = left.Denominator() * right.Denominator();
+    return { numerator, denominator };
+}
+
+// вычитание дробей
+Rational operator-(Rational left, Rational right) {
+    const int numerator = left.Numerator() * right.Denominator() - right.Numerator() * left.Denominator();
+    const int denominator = left.Denominator() * right.Denominator();
+    return { numerator, denominator };
+}
+
+// умножение дробей
+Rational operator*(Rational left, Rational right) {
+    const int numerator = left.Numerator() * right.Numerator();
+    const int denominator = left.Denominator() * right.Denominator();
+    return { numerator, denominator };
+}
+
+// унарный плюс
+Rational operator+(Rational drob) {
+    return drob;
+}
+
+// унарный минус
+Rational operator-(Rational drob) {
+    return { -drob.Numerator(),drob.Denominator()};
 }
 
 int main() {
-	// инициализируем поисковую систему, передавая стоп-слова в контейнере vector
-	const vector<string> stop_words_vector = { "и"s, "в"s, "на"s, ""s, "в"s };
-	SearchServer search_server1(stop_words_vector);
-	// инициализируем поисковую систему передавая стоп-слова в контейнере set
-	const set<string> stop_words_set = { "и"s, "в"s, "на"s };
-	SearchServer search_server2(stop_words_set);
-	// инициализируем поисковую систему строкой со стоп-словами, разделёнными пробелами
-	SearchServer search_server3("  и  в на   "s);
-	cout << "all cool" << endl;
+    Rational rat();
 }
