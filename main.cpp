76631348b0c45b81e8f81b9bd7d1cf2e7a7a2dbe
obegrand <obevan7777@yandex.ru>
2024-03-21 22:49:23 +0300

    chrono class

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 #include <vector>
 
+#include "log_duration.h"
+
 using namespace std;
-using namespace chrono;
 
 vector<int> ReverseVector(const vector<int>& source_vector) {
     vector<int> res;
@@ -31,35 +32,33 @@ void AppendRandom(vector<int>& v, int n) {
 }
 
 void Operate() {
+    LogDuration total("Total");
     vector<int> random_bits;
     static const int N = 1 << 17;
 
     {
-        const auto start_time = steady_clock::now();
+        LogDuration append_random("Append random");
+        // заполним вектор случайными числами 0 и 1
         AppendRandom(random_bits, N);
-        const auto end_time = steady_clock::now();
-        const auto dur = end_time - start_time;
-        cerr << "Append random: "s << chrono::duration_cast<chrono::milliseconds>(dur).count() << " ms"s << endl;
     }
-    const auto start_time = steady_clock::now();
-    vector<int> reversed_bits = ReverseVector(random_bits);
-    const auto end_time = steady_clock::now();
-    const auto dur = end_time - start_time;
-    cerr << "Reverse: "s << chrono::duration_cast<chrono::milliseconds>(dur).count() << " ms"s << endl;
+
+    vector<int> reversed_bits;
+    {
+        LogDuration reverse("Reverse");
+        // перевернём вектор задом наперёд
+        reversed_bits = ReverseVector(random_bits);
+    }
 
     {
-        const auto start_time = steady_clock::now();
+        LogDuration counting("Counting");
+        // посчитаем процент единиц на начальных отрезках вектора
         for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
             double rate = CountPops(reversed_bits, 0, i) * 100. / i;
             cout << "After "s << i << " bits we found "s << rate << "% pops"s << endl;
         }
-        const auto end_time = steady_clock::now();
-        const auto dur = end_time - start_time;
-        cerr << "Counting: "s << chrono::duration_cast<chrono::milliseconds>(dur).count() << " ms"s << endl;
     }
 }
 
 int main() {
     Operate();
-    return 0;
 }
