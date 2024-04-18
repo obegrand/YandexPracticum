c6a3acff862ccfa397e47e36be2cfa9b05ac7fff
obegrand <obevan7777@yandex.ru>
2024-04-18 23:22:17 +0300

    Тупой текстовый редактор#💀

---
+++ log_duration.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <chrono>
+#include <iostream>
+#include <string>
+
+class LogDuration {
+public:
+    LogDuration(const std::string name)
+        : name_(name)
+    { }
+
+    ~LogDuration() {
+        const auto end_time = std::chrono::steady_clock::now();
+        const auto dur = end_time - start_time_;
+        std::cerr << name_ << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << " ms" << std::endl;
+    }
+
+private:
+    const std::chrono::steady_clock::time_point start_time_ = std::chrono::steady_clock::now();
+    const std::string name_;
+};
--- main.cpp
+++ main.cpp
@@ -1,85 +1,167 @@
-#include <cassert>
+#include <iostream>
+#include <list>
+#include <string>
 #include <algorithm>
-#include <cstdlib>
 
-template <typename Type>
-class ArrayPtr {
+//#define DEBUG
+
+using namespace std;
+
+std::ostream& operator<<(std::ostream& ostr, const std::list<char>& list)
+{
+	for (auto& i : list) {
+		ostr << i;
+	}
+	return ostr;
+}
+
+class Editor {
 public:
-    // Инициализирует ArrayPtr нулевым указателем
-    ArrayPtr() = default;
-
-    // Создаёт в куче массив из size элементов типа Type.
-    // Если size == 0, поле raw_ptr_ должно быть равно nullptr
-    explicit ArrayPtr(size_t size) {
-        if (size == 0) return;
-        else raw_ptr_ = new Type[size];
-    }
-
-    // Конструктор из сырого указателя, хранящего адрес массива в куче либо nullptr
-    explicit ArrayPtr(Type* raw_ptr) noexcept : raw_ptr_(raw_ptr) { }
-
-    // Запрещаем копирование
-    ArrayPtr(const ArrayPtr&) = delete;
-
-    ~ArrayPtr() {
-        delete[] raw_ptr_;
-    }
-
-    // Запрещаем присваивание
-    ArrayPtr& operator=(const ArrayPtr&) = delete;
-
-    // Прекращает владением массивом в памяти, возвращает значение адреса массива
-    // После вызова метода указатель на массив должен обнулиться
-    [[nodiscard]] Type* Release() noexcept {
-        Type* tmp = raw_ptr_;
-        raw_ptr_ = nullptr;
-        return tmp;
-    }
-
-    // Возвращает ссылку на элемент массива с индексом index
-    Type& operator[](size_t index) noexcept {
-        return raw_ptr_[index];
-    }
-
-    // Возвращает константную ссылку на элемент массива с индексом index
-    const Type& operator[](size_t index) const noexcept {
-        return raw_ptr_[index];
-    }
-
-    // Возвращает true, если указатель ненулевой, и false в противном случае
-    explicit operator bool() const {
-        return raw_ptr_ != nullptr;
-    }
-
-    // Возвращает значение сырого указателя, хранящего адрес начала массива
-    Type* Get() const noexcept {
-        return raw_ptr_;
-    }
-
-    // Обменивается значениям указателя на массив с объектом other
-    void swap(ArrayPtr& other) noexcept {
-        std::swap(raw_ptr_, other.raw_ptr_);
-    }
-
+	string get_ptr() {
+		return string(1,*cursor_);
+	}
+	
+	Editor() {
+		line_.insert(line_.begin(), '|');
+		cursor_ = find(line_.begin(), line_.end(), '|');
+	}
+	// сдвинуть курсор влево
+	void Left() {
+		if (cursor_ == line_.begin())return;
+		line_.erase(cursor_--);
+		line_.insert(cursor_, '|');
+		--cursor_;
+	}
+	// сдвинуть курсор вправо 
+	void Right() {
+		if (cursor_ == --line_.end())return;
+		line_.erase(cursor_++);
+		line_.insert(++cursor_, '|');
+		--cursor_;
+	}
+	// вставить символ token
+	void Insert(char token) {
+		line_.insert(cursor_, token);
+	}
+	// вырезать не более tokens символов, начиная с текущей позиции курсора
+	void Cut(size_t tokens = 1) {
+		Copy(tokens);
+		if (cursor_ != line_.end()) ++cursor_;
+		while (cursor_ != line_.end() && tokens > 0) {
+			line_.erase(cursor_++);
+			--tokens;
+		}
+		cursor_ = find(line_.begin(), line_.end(), '|');
+	}
+	// cкопировать не более tokens символов, начиная с текущей позиции курсора
+	void Copy(size_t tokens = 1) {
+		buffer.clear();
+		if (cursor_ != line_.end()) ++cursor_;
+		while (cursor_ != line_.end() && tokens > 0) {
+			buffer.insert(buffer.end(), *cursor_++);
+			--tokens;
+		}
+		cursor_ = find(line_.begin(), line_.end(), '|');
+	}
+	// вставить содержимое буфера в текущую позицию курсора
+	void Paste() {
+		for (char letter : buffer) {
+			line_.insert(cursor_, letter);
+		}
+	}
+	// получить текущее содержимое текстового редактора
+	string GetText() const {
+		string result;
+		for (const auto& ch : line_) {
+			if (ch != '|') { // Исключаем символ '|' из результата
+				result += ch;
+			}
+		}
+		return result;
+	}
 private:
-    Type* raw_ptr_ = nullptr;
+	list<char> line_;
+	list<char> buffer;
+	std::list<char>::iterator cursor_;
 };
 
+int test() {
+
+	Editor editor;
+	const string text = "hello, world"s;
+	for (char c : text) {
+		editor.Insert(c);
+	}
+	// Текущее состояние редактора: `hello, world|`
+#ifdef DEBUG
+	{
+		string right = "hello, world|"s;
+		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
+	}
+#endif DEBUG
+	for (size_t i = 0; i < text.size(); ++i) {
+		editor.Left();
+	}
+	// Текущее состояние редактора: `|hello, world`
+#ifdef DEBUG
+	{
+		string right = "|hello, world"s;
+		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
+	}
+#endif DEBUG
+	editor.Cut(7);
+	// Текущее состояние редактора: `|world`
+	// в буфере обмена находится текст `hello, `
+#ifdef DEBUG
+	{
+		string right = "|world"s;
+		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
+	}
+#endif DEBUG
+	for (size_t i = 0; i < 5; ++i) {
+		editor.Right();
+		//cout << "\t" << editor.get_ptr() << endl;
+	}
+	// Текущее состояние редактора: `world|`
+#ifdef DEBUG
+	{
+		string right = "world|"s;
+		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
+	}
+#endif DEBUG
+	editor.Insert(',');
+	editor.Insert(' ');
+	// Текущее состояние редактора: `world, |`
+#ifdef DEBUG
+	{
+		string right = "world, |"s;
+		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
+	}
+#endif DEBUG
+	editor.Paste();
+	// Текущее состояние редактора: `world, hello, |`
+	editor.Left();
+	editor.Left();
+#ifdef DEBUG
+	{
+		string right = "world, hello|, "s;
+		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
+	}
+#endif DEBUG
+	//Текущее состояние редактора: `world, hello|, `
+	editor.Cut(3);  // Будут вырезаны 2 символа
+	// Текущее состояние редактора: `world, hello|`
+#ifdef DEBUG
+	{
+		string right = "world, hello|"s;
+		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
+	}
+#endif DEBUG
+	cout << editor.GetText();
+	return 0;
+}
+
 int main() {
-    ArrayPtr<int> numbers(10);
-    const auto& const_numbers = numbers;
-
-    numbers[2] = 42;
-    assert(const_numbers[2] == 42);
-    assert(&const_numbers[2] == &numbers[2]);
-
-    assert(numbers.Get() == &numbers[0]);
-
-    ArrayPtr<int> numbers_2(5);
-    numbers_2[2] = 43;
-
-    numbers.swap(numbers_2);
-
-    assert(numbers_2[2] == 42);
-    assert(numbers[2] == 43);
+	system("color A");
+	test();
 }
