78e0087e16d79079dafda01b60b601baa01e6e3e
obegrand <obevan7777@yandex.ru>
2024-04-20 19:14:10 +0300

    StackVector

--- log_duration.h
+++ log_duration.h
@@ -1,21 +1,36 @@
 #pragma once
+
 #include <chrono>
 #include <iostream>
-#include <string>
+
+#define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
+#define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
+#define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
+#define LOG_DURATION(x) LogDuration UNIQUE_VAR_NAME_PROFILE(x)
+#define LOG_DURATION_STREAM(x, y) LogDuration UNIQUE_VAR_NAME_PROFILE(x, y)
 
 class LogDuration {
 public:
-    LogDuration(const std::string name)
-        : name_(name)
-    { }
+    // заменим имя типа std::chrono::steady_clock
+    // с помощью using для удобства
+    using Clock = std::chrono::steady_clock;
+
+    LogDuration(const std::string& id, std::ostream& dst_stream = std::cerr)
+        : id_(id)
+        , dst_stream_(dst_stream) {
+    }
 
     ~LogDuration() {
-        const auto end_time = std::chrono::steady_clock::now();
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
         const auto dur = end_time - start_time_;
-        std::cerr << name_ << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << " ms" << std::endl;
+        dst_stream_ << id_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
     }
 
 private:
-    const std::chrono::steady_clock::time_point start_time_ = std::chrono::steady_clock::now();
-    const std::string name_;
+    const std::string id_;
+    const Clock::time_point start_time_ = Clock::now();
+    std::ostream& dst_stream_;
 };
--- main.cpp
+++ main.cpp
@@ -1,167 +1,116 @@
-#include <iostream>
-#include <list>
-#include <string>
-#include <algorithm>
+#include "log_duration.h"
+#include "my_assert.h"
+#include "stack_vector.h"
 
-//#define DEBUG
+#include <iostream>
+#include <random>
+#include <stdexcept>
 
 using namespace std;
 
-std::ostream& operator<<(std::ostream& ostr, const std::list<char>& list)
-{
-	for (auto& i : list) {
-		ostr << i;
-	}
-	return ostr;
+void TestConstruction() {
+    StackVector<int, 10> v;
+    assert(v.Size() == 0u);
+    assert(v.Capacity() == 10u);
+
+    StackVector<int, 8> u(5);
+    assert(u.Size() == 5u);
+    assert(u.Capacity() == 8u);
+
+    try {
+        StackVector<int, 10> u(50);
+        cout << "Expect invalid_argument for too large size"s << endl;
+        assert(false);
+    } catch (invalid_argument&) {
+    } catch (...) {
+        cout << "Expect invalid_argument for too large size"s << endl;
+        assert(false);
+    }
 }
 
-class Editor {
-public:
-	string get_ptr() {
-		return string(1,*cursor_);
-	}
-	
-	Editor() {
-		line_.insert(line_.begin(), '|');
-		cursor_ = find(line_.begin(), line_.end(), '|');
-	}
-	// сдвинуть курсор влево
-	void Left() {
-		if (cursor_ == line_.begin())return;
-		line_.erase(cursor_--);
-		line_.insert(cursor_, '|');
-		--cursor_;
-	}
-	// сдвинуть курсор вправо 
-	void Right() {
-		if (cursor_ == --line_.end())return;
-		line_.erase(cursor_++);
-		line_.insert(++cursor_, '|');
-		--cursor_;
-	}
-	// вставить символ token
-	void Insert(char token) {
-		line_.insert(cursor_, token);
-	}
-	// вырезать не более tokens символов, начиная с текущей позиции курсора
-	void Cut(size_t tokens = 1) {
-		Copy(tokens);
-		if (cursor_ != line_.end()) ++cursor_;
-		while (cursor_ != line_.end() && tokens > 0) {
-			line_.erase(cursor_++);
-			--tokens;
-		}
-		cursor_ = find(line_.begin(), line_.end(), '|');
-	}
-	// cкопировать не более tokens символов, начиная с текущей позиции курсора
-	void Copy(size_t tokens = 1) {
-		buffer.clear();
-		if (cursor_ != line_.end()) ++cursor_;
-		while (cursor_ != line_.end() && tokens > 0) {
-			buffer.insert(buffer.end(), *cursor_++);
-			--tokens;
-		}
-		cursor_ = find(line_.begin(), line_.end(), '|');
-	}
-	// вставить содержимое буфера в текущую позицию курсора
-	void Paste() {
-		for (char letter : buffer) {
-			line_.insert(cursor_, letter);
-		}
-	}
-	// получить текущее содержимое текстового редактора
-	string GetText() const {
-		string result;
-		for (const auto& ch : line_) {
-			if (ch != '|') { // Исключаем символ '|' из результата
-				result += ch;
-			}
-		}
-		return result;
-	}
-private:
-	list<char> line_;
-	list<char> buffer;
-	std::list<char>::iterator cursor_;
-};
+void TestPushBack() {
+    StackVector<int, 5> v;
+    for (size_t i = 0; i < v.Capacity(); ++i) {
+        v.PushBack(i);
+    }
 
-int test() {
+    try {
+        v.PushBack(0);
+        cout << "Expect overflow_error for PushBack in full vector"s << endl;
+        assert(false);
+    } catch (overflow_error&) {
+    } catch (...) {
+        cout << "Unexpected exception for PushBack in full vector"s << endl;
+        assert(false);
+    }
+}
 
-	Editor editor;
-	const string text = "hello, world"s;
-	for (char c : text) {
-		editor.Insert(c);
-	}
-	// Текущее состояние редактора: `hello, world|`
-#ifdef DEBUG
-	{
-		string right = "hello, world|"s;
-		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
-	}
-#endif DEBUG
-	for (size_t i = 0; i < text.size(); ++i) {
-		editor.Left();
-	}
-	// Текущее состояние редактора: `|hello, world`
-#ifdef DEBUG
-	{
-		string right = "|hello, world"s;
-		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
-	}
-#endif DEBUG
-	editor.Cut(7);
-	// Текущее состояние редактора: `|world`
-	// в буфере обмена находится текст `hello, `
-#ifdef DEBUG
-	{
-		string right = "|world"s;
-		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
-	}
-#endif DEBUG
-	for (size_t i = 0; i < 5; ++i) {
-		editor.Right();
-		//cout << "\t" << editor.get_ptr() << endl;
-	}
-	// Текущее состояние редактора: `world|`
-#ifdef DEBUG
-	{
-		string right = "world|"s;
-		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
-	}
-#endif DEBUG
-	editor.Insert(',');
-	editor.Insert(' ');
-	// Текущее состояние редактора: `world, |`
-#ifdef DEBUG
-	{
-		string right = "world, |"s;
-		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
-	}
-#endif DEBUG
-	editor.Paste();
-	// Текущее состояние редактора: `world, hello, |`
-	editor.Left();
-	editor.Left();
-#ifdef DEBUG
-	{
-		string right = "world, hello|, "s;
-		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
-	}
-#endif DEBUG
-	//Текущее состояние редактора: `world, hello|, `
-	editor.Cut(3);  // Будут вырезаны 2 символа
-	// Текущее состояние редактора: `world, hello|`
-#ifdef DEBUG
-	{
-		string right = "world, hello|"s;
-		editor.GetText() == right ? cout << "Test ok - [" << right << "]" << endl : cout << "[" << right << "] - [" << editor.GetText() << "]" <<__LINE__<< endl;
-	}
-#endif DEBUG
-	cout << editor.GetText();
-	return 0;
+void TestPopBack() {
+    StackVector<int, 5> v;
+    for (size_t i = 1; i <= v.Capacity(); ++i) {
+        v.PushBack(i);
+    }
+    for (int i = v.Size(); i > 0; --i) {
+        assert(v.PopBack() == i);
+    }
+
+    try {
+        v.PopBack();
+        cout << "Expect underflow_error for PopBack from empty vector"s << endl;
+        assert(false);
+    } catch (underflow_error&) {
+    } catch (...) {
+        cout << "Unexpected exception for PopBack from empty vector"s << endl;
+        assert(false);
+    }
 }
 
 int main() {
-	system("color A");
-	test();
+    TestConstruction();
+    TestPushBack();
+    TestPopBack();
+
+    cerr << "Running benchmark..."s << endl;
+    const size_t max_size = 2500;
+
+    default_random_engine re;
+    uniform_int_distribution<int> value_gen(1, max_size);
+
+    vector<vector<int>> test_data(50000);
+    for (auto& cur_vec : test_data) {
+        cur_vec.resize(value_gen(re));
+        for (int& x : cur_vec) {
+            x = value_gen(re);
+        }
+    }
+
+    {
+        LOG_DURATION("vector w/o reserve");
+        for (auto& cur_vec : test_data) {
+            vector<int> v;
+            for (int x : cur_vec) {
+                v.push_back(x);
+            }
+        }
+    }
+    {
+        LOG_DURATION("vector with reserve");
+        for (auto& cur_vec : test_data) {
+            vector<int> v;
+            v.reserve(cur_vec.size());
+            for (int x : cur_vec) {
+                v.push_back(x);
+            }
+        }
+    }
+    {
+        LOG_DURATION("StackVector");
+        for (auto& cur_vec : test_data) {
+            StackVector<int, max_size> v;
+            for (int x : cur_vec) {
+                v.PushBack(x);
+            }
+        }
+    }
+    cerr << "Done"s << endl;
 }
