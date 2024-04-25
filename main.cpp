113111c5088bd2014ed911e69f80a17ec94c1a16
obegrand <obevan7777@yandex.ru>
2024-04-26 00:26:36 +0300

    vector<Sentence<Token>> SplitIntoSentences(vector<Token> tokens)  через move-сематику

--- main.cpp
+++ main.cpp
@@ -1,52 +1,77 @@
-#include "log_duration.h"
-
-// Tests
-#include "simple_vector.h"
-#include "tests.h"
+#include <algorithm>
 #include <cassert>
-#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-void TestReserveConstructor() {
-    cout << "TestReserveConstructor"s << endl;
-    SimpleVector<int> v(Reserve(5));
-    assert(v.GetCapacity() == 5);
-    assert(v.IsEmpty());
-    cout << "Done!"s << endl;
+// Объявляем Sentence<Token> для произвольного типа Token
+// синонимом vector<Token>.
+// Благодаря этому в качестве возвращаемого значения
+// функции можно указать не малопонятный вектор векторов,
+// а вектор предложений — vector<Sentence<Token>>.
+template <typename Token>
+using Sentence = vector<Token>;
+
+template <typename TokenForwardIt>
+TokenForwardIt FindSentenceEnd(TokenForwardIt tokens_begin, TokenForwardIt tokens_end) {
+	const TokenForwardIt before_sentence_end
+		= adjacent_find(tokens_begin, tokens_end, [](const auto& left_token, const auto& right_token) {
+		return left_token.IsEndSentencePunctuation() && !right_token.IsEndSentencePunctuation();
+			});
+	return before_sentence_end == tokens_end ? tokens_end : next(before_sentence_end);
 }
 
-void TestReserveMethod() {
-    cout << "TestReserveMethod"s << endl;
-    SimpleVector<int> v;
-    // зарезервируем 5 мест в векторе
-    v.Reserve(5);
-    assert(v.GetCapacity() == 5);
-    assert(v.IsEmpty());
+// Класс Token имеет метод bool IsEndSentencePunctuation() const
+template <typename Token>
+vector<Sentence<Token>> SplitIntoSentences(vector<Token> tokens) {
+	vector<Sentence<Token>> result;
+	auto iterator = tokens.begin();
+	while (iterator != tokens.end()) {
+		const auto it_comma = FindSentenceEnd(iterator, tokens.end());
+		Sentence<Token> sentence;
+		for (; iterator != it_comma; iterator++) {
+			sentence.push_back(move(*iterator));
+		}
+		result.push_back(move(sentence));
+	}
+	return result;
+}
 
-    // попытаемся уменьшить capacity до 1
-    v.Reserve(1);
-    // capacity должно остаться прежним
-    assert(v.GetCapacity() == 5);
-    // поместим 10 элементов в вектор
-    for (int i = 0; i < 10; ++i) {
-        v.PushBack(i);
-    }
-    assert(v.GetSize() == 10);
-    // увеличим capacity до 100
-    v.Reserve(100);
-    // проверим, что размер не поменялся
-    assert(v.GetSize() == 10);
-    assert(v.GetCapacity() == 100);
-    // проверим, что элементы на месте
-    for (int i = 0; i < 10; ++i) {
-        assert(v[i] == i);
-    }
-    cout << "Done!"s << endl;
+struct TestToken {
+	string data;
+	bool is_end_sentence_punctuation = false;
+
+	bool IsEndSentencePunctuation() const {
+		return is_end_sentence_punctuation;
+	}
+	bool operator==(const TestToken& other) const {
+		return data == other.data && is_end_sentence_punctuation == other.is_end_sentence_punctuation;
+	}
+};
+
+ostream& operator<<(ostream& stream, const TestToken& token) {
+	return stream << token.data;
+}
+
+// Тест содержит копирования объектов класса TestToken.
+// Для проверки отсутствия копирований в функции SplitIntoSentences
+// необходимо написать отдельный тест.
+void TestSplitting() {
+	assert(SplitIntoSentences(vector<TestToken>({ {"Split"s}, {"into"s}, {"sentences"s}, {"!"s} }))
+		== vector<Sentence<TestToken>>({ {{"Split"s}, {"into"s}, {"sentences"s}, {"!"s}} }));
+
+	assert(SplitIntoSentences(vector<TestToken>({ {"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true} }))
+		== vector<Sentence<TestToken>>({ {{"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true}} }));
+
+	assert(SplitIntoSentences(vector<TestToken>(
+		{ {"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true}, {"!"s, true}, {"Without"s}, {"copies"s}, {"."s, true} }))
+		== vector<Sentence<TestToken>>({
+			{{"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true}, {"!"s, true}},
+			{{"Without"s}, {"copies"s}, {"."s, true}},
+			}));
 }
 
 int main() {
-    LOG_DURATION("main");
-    TestReserveConstructor();
-    TestReserveMethod();
+	TestSplitting();
 }
