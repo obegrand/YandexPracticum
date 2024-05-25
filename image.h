498957339a88d2cf5a47b55f847a46f47f0d92f9
obegrand <obevan7777@yandex.ru>
2024-05-25 11:47:53 +0300

    std::vector<CoWTile> tiles_

---
+++ image.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include <ostream>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+#include "cow.h"
+#include "geom.h"
+#include "tile.h"
+
+class Image {
+public:
+	/**
+	 * Конструирует изображение заданного размера. Если размеры не являются положительными,
+	 * выбрасывает исключение std::out_of_range.
+	 */
+	explicit Image(Size size, char color = ' ')
+		: size_{ size.width > 0 && size.height > 0 ? size
+												  : throw std::out_of_range("Invalid image size") }
+		, size_in_tiles_{ (size_.width + Tile::SIZE - 1) / Tile::SIZE,
+						 (size_.height + Tile::SIZE - 1) / Tile::SIZE }
+		, tiles_{ static_cast<size_t>(size_in_tiles_.width * size_in_tiles_.height),
+				 CoWTile{Tile{color}} } {
+	}
+
+	// Возвращает размер изображения в пикселях.
+	Size GetSize() const noexcept {
+		return size_;
+	}
+
+	/**
+	 * Возвращает «цвет» пикселя в указанных координатах.Если координаты выходят за пределы
+	 * изображения, возвращает «пробел».
+	 */
+	char GetPixel(Point p) const noexcept {
+		if (IsPointInSize(p, size_)) {
+			//return tiles_[GetTileIndex(p)]->GetPixel({ p.x % Tile::SIZE, p.y % Tile::SIZE });
+
+			int tile_index = GetTileIndex(p);
+			if (tile_index >= 0 && tile_index < tiles_.size()) {
+			    auto tmp = tiles_[tile_index]->GetPixel({ p.x % Tile::SIZE, p.y % Tile::SIZE });
+			    return tmp;
+			}
+		}
+		else return ' ';
+	}
+
+	/**
+	 * Задаёт «цвет» пикселя в указанных координатах. Если координаты выходят за пределы изображения
+	 * действие игнорируется.
+	 */
+	void SetPixel(Point p, char color) {
+		if (IsPointInSize(p, size_)) {
+			tiles_[GetTileIndex(p)].Write(
+				[&](Tile& tile) {
+					tile.SetPixel({ p.x % Tile::SIZE, p.y % Tile::SIZE }, color);
+				}
+			);
+		}
+	}
+
+private:
+	using CoWTile = CoW<Tile>;
+
+	// Возвращает порядковый номер тайла, в котором находится пиксель с координатами p
+	int GetTileIndex(Point p) const noexcept {
+		assert((p.x >= 0) && (p.x < size_.width) && (p.y >= 0) && (p.y < size_.height));
+		return (p.y / Tile::SIZE) * size_in_tiles_.width + (p.x / Tile::SIZE);
+	}
+
+	Size size_;                   // Размер изображения в пикселях.
+	Size size_in_tiles_;          // Размер изображения в тайлах.
+	std::vector<CoWTile> tiles_;  // Тайлы изображения (строка за строкой).
+};
+
+inline void Print(const Image& img, std::ostream& out) {
+	const auto size = img.GetSize();
+	for (int y = 0; y < size.height; ++y) {
+		for (int x = 0; x < size.width; ++x) {
+			out.put(img.GetPixel({ x, y }));
+		}
+		out.put('\n');
+	}
+}
+
+inline Image LoadImage(const std::string& pixels) {
+	std::istringstream s(pixels);
+	Size size;
+	std::string line;
+	while (std::getline(s, line)) {
+		size.width = std::max(size.width, static_cast<int>(line.length()));
+		++size.height;
+	}
+
+	Image img(size);
+
+	s = std::istringstream(pixels);
+	for (int y = 0; y < size.height; ++y) {
+		if (!std::getline(s, line)) break;
+
+		int x = 0;
+		for (char ch : line) {
+			img.SetPixel({ x++, y }, ch);
+		}
+	}
+
+	return img;
+}
--- main.cpp
+++ main.cpp
@@ -1,48 +1,28 @@
-#include <cassert>
+#include <cmath>
+#include <iostream>
 
-#include "bimap.h"
+#include "drawer.h"
+
+constexpr int STAR_COUNT = 20;
 
 int main() {
-    using namespace std::literals;
-
-    BiMap bimap;
     {
-        const bool cat_added = bimap.Add("Cat"sv, "Koshka"sv);
-
-        assert(cat_added);
-
-        assert(bimap.FindValue("Cat"sv) == "Koshka"sv);
-        assert(bimap.FindKey("Koshka"sv) == "Cat"sv);
-        assert(!bimap.FindKey("Cat"sv));
-        assert(!bimap.FindValue("Koshka"sv));
-
-        const bool kitty_added = bimap.Add("Cat"sv, "Kitty"sv);
-
-        assert(!kitty_added);
-        assert(bimap.FindValue("Cat"sv) == "Koshka"sv);
-        assert(!bimap.FindValue("Kitty"sv));
+        Image img = LoadImage(
+            " CCCC             \n"
+            "CC  CC   ##    ## \n"
+            "CC      ####  ####\n"
+            "CC  CC   ##    ## \n"
+            " CCCC             \n");
+        Print(img, std::cout);
     }
 
-    BiMap bimap_copy(bimap);
-    assert(bimap_copy.FindValue("Cat"sv) == "Koshka"sv);
-    assert(bimap_copy.FindKey("Koshka"sv) == "Cat"sv);
+    {
+        Image img{{30, 20}, '.'};
 
-    bimap_copy.Add("Dog"sv, "Sobaka"sv);
-    assert(bimap_copy.FindValue("Dog"sv) == "Sobaka"sv);
-    assert(bimap_copy.FindKey("Sobaka"sv) == "Dog"sv);
-    assert(!bimap.FindKey("Sobaka"sv));
-    assert(!bimap.FindValue("Dog"sv));
+        DrawLine(img, {3, 2}, {26, 5}, '#');
+        DrawLine(img, {26, 5}, {21, 18}, '#');
+        DrawLine(img, {21, 18}, {3, 2}, '#');
 
-    bimap = std::move(bimap_copy);
-    assert(bimap.FindValue("Dog"sv) == "Sobaka"sv);
-    assert(bimap.FindKey("Sobaka"sv) == "Dog"sv);
-
-    BiMap moved_bimap(std::move(bimap));
-    assert(moved_bimap.FindValue("Dog"sv) == "Sobaka"sv);
-    assert(moved_bimap.FindKey("Sobaka"sv) == "Dog"sv);
-
-    BiMap bimap_to_assign;
-    bimap_to_assign = moved_bimap;
-    assert(bimap_to_assign.FindValue("Dog"sv) == "Sobaka"sv);
-    assert(bimap_to_assign.FindKey("Sobaka"sv) == "Dog"sv);
+        Print(img, std::cout);
+    }
 }
