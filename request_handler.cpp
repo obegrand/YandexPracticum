635c4ffff6b3a5502f8726162718581dacf7a4ec
obegrand <obevan7777@yandex.ru>
2024-07-06 16:02:46 +0300

    11 спринт рабочая версия с интеграцей json::Builder

--- request_handler.cpp
+++ request_handler.cpp
@@ -1,5 +1,6 @@
 #include "request_handler.h"
 #include <sstream>
+#include "json_builder.h"
 
 /*
  * Здесь можно было бы разместить код обработчика запросов к базе, содержащего логику, которую не
@@ -17,8 +18,7 @@ RequestHandler::RequestHandler(JsonReader& requests, catalogue::TransportCatalog
 void RequestHandler::ProcessRequests() const {
 	if (requests_.GetStatRequests() == nullptr) return;
 	json::Array result;
-	const json::Array& stat_requests_arr = requests_.GetStatRequests().AsArray();
-	for (const auto& stat_request : stat_requests_arr) {
+	for (const auto& stat_request : requests_.GetStatRequests().AsArray()) {
 		std::string type = stat_request.AsMap().at("type").AsString();
 		if (type == "Stop") {
 			result.push_back(PrintStop(stat_request.AsMap()));
@@ -35,47 +35,71 @@ void RequestHandler::ProcessRequests() const {
 }
 
 const json::Node RequestHandler::PrintBus(const json::Dict& request_map) const {
-	json::Dict result;
+	json::Node result;
 	const std::string& bus_name = request_map.at("name").AsString();
-	result["request_id"] = request_map.at("id").AsInt();
+	const int id = request_map.at("id").AsInt();
 	if (!db_.ContainsBus(bus_name)) {
-		result["error_message"] = json::Node{ static_cast<std::string>("not found") };
+		result = json::Builder{}
+				.StartDict()
+					.Key("request_id").Value(id)
+					.Key("error_message").Value("not found")
+				.EndDict()
+			.Build();
 	}
 	else {
 		catalogue::BusStat bus_stat = GetBusStat(bus_name).value();
-		result["curvature"] = bus_stat.curvature;
-		result["route_length"] = bus_stat.route_length;
-		result["stop_count"] = bus_stat.stops_count;
-		result["unique_stop_count"] = bus_stat.unique_stops_count;
+		result = json::Builder{}
+				.StartDict()
+					.Key("curvature").Value(bus_stat.curvature)
+					.Key("route_length").Value(bus_stat.route_length)
+					.Key("stop_count").Value(bus_stat.stops_count)
+					.Key("unique_stop_count").Value(bus_stat.unique_stops_count)
+					.Key("request_id").Value(id)
+				.EndDict()
+			.Build();
 	}
-	return json::Node{ result };
+	return result;
 }
 
 const json::Node RequestHandler::PrintStop(const json::Dict& request_map) const {
-	json::Dict result;
+	json::Node result;
 	const std::string& stop_name = request_map.at("name").AsString();
-	result["request_id"] = request_map.at("id").AsInt();
+	const int id = request_map.at("id").AsInt();
 	if (!db_.ContainsStop(stop_name)) {
-		result["error_message"] = json::Node{ static_cast<std::string>("not found") };
+		result = json::Builder{}
+				.StartDict()
+					.Key("error_message").Value("not found")
+					.Key("request_id").Value(id)
+				.EndDict()
+			.Build();
 	}
 	else {
 		json::Array buses;
-		for (auto& bus : GetBusesByStop(stop_name)) {
+		for (auto& bus : db_.GetBusesByStop(stop_name)) {
 			buses.push_back(std::string(bus));
 		}
-		result["buses"] = buses;
+		result = json::Builder{}
+				.StartDict()
+					.Key("buses").Value(buses)
+					.Key("request_id").Value(id)
+				.EndDict()
+			.Build();
 	}
-	return json::Node{ result };
+	return result;
 }
 
 json::Node RequestHandler::PrintMap(const json::Dict& request_map, render::MapRender& map_render) const {
-	json::Dict result;
-	result["request_id"] = request_map.at("id").AsInt();
+	json::Node result;
+	const int id = request_map.at("id").AsInt();
 	std::stringstream output;
 	map_render.CreateMap(db_.GetAllBusesSorted()).Render(output);
-	result["map"] = json::Node{ output.str() };
-
-	return json::Node{ result };
+	result = json::Builder{}
+			.StartDict()
+				.Key("map").Value(output.str())
+				.Key("request_id").Value(id)
+			.EndDict()
+		.Build();
+	return result;
 }
 
 std::optional<catalogue::BusStat> RequestHandler::GetBusStat(const std::string_view bus_number) const {
@@ -91,8 +115,4 @@ std::optional<catalogue::BusStat> RequestHandler::GetBusStat(const std::string_v
 	bus_stat.route_length = distance;
 	bus_stat.curvature = distance / db_.ComputeGeoDistance(bus.stop_names);
 	return bus_stat;
-}
-
-const std::set<std::string_view> RequestHandler::GetBusesByStop(std::string_view stop_name) const {
-	return db_.GetBusesByStop(stop_name);
 }
--- request_handler.h
+++ request_handler.h
@@ -8,27 +8,6 @@
  * Если вы затрудняетесь выбрать, что можно было бы поместить в этот файл,
  * можете оставить его пустым.
  */
-
- //class RequestHandler {
- //public:
- //    // MapRenderer понадобится в следующей части итогового проекта
- //    // RequestHandler(const TransportCatalogue& db, const renderer::MapRenderer& renderer);
- //
- //    // Возвращает информацию о маршруте (запрос Bus)
- //    std::optional<catalogue::BusStat> GetBusStat(const std::string_view& bus_name) const;
- //
- //    // Возвращает маршруты, проходящие через
- //    const std::unordered_set<catalogue::Bus>* GetBusesByStop(const std::string_view& stop_name) const;
- //
- //    // Этот метод будет нужен в следующей части итогового проекта
- //    // svg::Document RenderMap() const;
- //
- //private:
- //    // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
- //    const catalogue::TransportCatalogue& db_;
- //    // const renderer::MapRenderer& renderer_;
- //};
-
 #pragma once
 #include <optional>
 #include <unordered_set>
@@ -48,8 +27,6 @@ public:
 	json::Node PrintMap(const json::Dict& request_map, render::MapRender& map_render) const;
 
 	std::optional<catalogue::BusStat> GetBusStat(const std::string_view bus_number) const;
-
-	const std::set<std::string_view> GetBusesByStop(std::string_view stop_name) const;
 
 private:
 	const JsonReader& requests_;
