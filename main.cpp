54b53c96284fad74bfbe9030ee0bf8f06bc724e9
obegrand <obevan7777@yandex.ru>
2024-07-23 10:33:58 +0300

    merge different containers

--- main.cpp
+++ main.cpp
@@ -1,62 +1,61 @@
-#include <algorithm>
-#include <iomanip>
 #include <iostream>
-#include <string>
-#include <unordered_map>
-#include <map>
-#include <numeric>
+#include <list>
+#include <set>
+#include <string_view>
+#include <vector>
 
-class Ebook_sim {
-public:
-	void Read(size_t user_id, uint16_t page_count) {
-		if (users_read_progression_.contains(user_id))
-			page_count_by_user_[users_read_progression_[user_id]]--; // delete outdated data
+using namespace std;
 
-		page_count_by_user_[page_count]++;
-		users_read_progression_[std::move(user_id)] = std::move(page_count);
-	}
+template <typename InputIt1, typename InputIt2>
+void Merge(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, std::ostream& out) {
+    while (first1 != last1 && first2 != last2) {
+        if (std::less<>()(*first1, *first2)) {
+            out << *first1++ << std::endl;
+        }
+        else {
+            out << *first2++ << std::endl;
+        }
+    }
+    while (first1 != last1) {
+        out << *first1++ << std::endl;
+    }
+    while (first2 != last2) {
+        out << *first2++ << std::endl;
+    }
+}
 
-	float Cheer(size_t user_id) {
-		size_t total_users = users_read_progression_.size();
+template <typename Container1, typename Container2>
+void MergeSomething(const Container1& src1, const Container2& src2, std::ostream& out) {
+    Merge(std::cbegin(src1), std::cend(src1), std::cbegin(src2), std::cend(src2), out);
+}
 
-		if (!users_read_progression_.contains(user_id)) return 0; // user not found
-		if (total_users == 1) return 1; // hi single user
-
-		size_t less_than_user = std::accumulate(page_count_by_user_.begin(),
-			page_count_by_user_.lower_bound(users_read_progression_[std::move(user_id)]), 0,
-			[](size_t sum, const std::pair<uint16_t, size_t>& p) {
-				return sum + p.second;
-			});
-
-		return static_cast<float>(less_than_user) / (total_users - 1);
-	}
-
-private:
-	std::unordered_map<size_t, uint16_t> users_read_progression_; // <user_id, read_progress>
-	std::map<uint16_t, size_t> page_count_by_user_; // <read_progress, how many users on this stage>
-};
-
+template <typename T>
+void MergeHalves(const std::vector<T>& src, std::ostream& out) {
+    size_t mid = (src.size() + 1) / 2;
+    Merge(src.cbegin(), src.cbegin() + mid, src.cbegin() + mid, src.cend(), out);
+}
 
 int main() {
-	size_t query_size;
-	std::cin >> query_size;
-	Ebook_sim e_book;
+    vector<int> v1{ 60, 70, 80, 90 };
+    vector<int> v2{ 65, 75, 85, 95 };
+    vector<int> combined{ 60, 70, 80, 90, 65, 75, 85, 95 };
+    list<double> my_list{ 0.1, 72.5, 82.11, 1e+30 };
+    string_view my_string = "ACNZ"sv;
+    set<unsigned> my_set{ 20u, 77u, 81u };
 
-	for (size_t i = 0; i < query_size; i++) {
-		std::string query_type;
-		std::cin >> query_type;
-		if (query_type == "READ") {
-			size_t user_id;
-			uint16_t page_count;
-			std::cin >> user_id >> page_count;
-			e_book.Read(user_id, page_count);
-		}
-		else if (query_type == "CHEER") {
-			size_t user_id;
-			std::cin >> user_id;
-			std::cout << std::setprecision(6) << e_book.Cheer(user_id) << std::endl;
-		}
-		else throw std::logic_error("invalid command");
-	}
-	return 0;
+    // пока функция MergeSomething реализована пока только для векторов
+    cout << "Merging vectors:"sv << endl;
+    MergeSomething(v1, v2, cout);
+
+    cout << "Merging vector and list:"sv << endl;
+    MergeSomething(v1, my_list, cout);
+
+    cout << "Merging string and list:"sv << endl;
+    MergeSomething(my_string, my_list, cout);
+
+    cout << "Merging set and vector:"sv << endl;
+    MergeSomething(my_set, v2, cout);
+
+    cout << "Merging vector halves:"sv << endl;
+    MergeHalves(combined, cout);
 }
